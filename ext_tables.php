9883628d0b0a4189a25371d253e234629c334d64
daKmoR <thomas.allmer@webteam.at>
2012-11-26 03:01:45 +0100

    [BUGFIX] proper TCA adding and create checkboxes manually

--- ext_tables.php
+++ ext_tables.php
@@ -11,25 +11,21 @@ Tx_Extbase_Utility_Extension::registerPlugin(
 
 t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Register using extbase');
 
-$TCA['fe_users']['interface'] = array(
-	'showRecordFieldList' => 'gtc,username,password,usergroup,lockToDomain,name,first_name,middle_name,last_name,title,company,address,zip,city,country,email,www,telephone,fax,disable,starttime,endtime,lastlogin',
-);
-$TCA['fe_users']['columns']['gtc'] = array(
-	'exclude'	=> 0,
-	'label'		=> 'AGB',
-	'config'	=> array(
-		'type'	=> 'check',
+// fe_users modified
+$tempCols = array(
+	'gtc' => array(
+		'exclude'	=> 0,
+		'label'		=> 'AGB',
+		'config'	=> array(
+			'type'	=> 'check',
+		)
 	)
 );
-$TCA['fe_users']['types'] = array(
-		'0' => array('showitem' => '
-			disable,gtc,username;;;;1-1-1, password, usergroup, lastlogin;;;;1-1-1,
-			--div--;LLL:EXT:cms/locallang_tca.xml:fe_users.tabs.personelData, company;;1;;1-1-1, name;;2;;2-2-2, address, zip, city, country, telephone, fax, email, www, image;;;;2-2-2,
-			--div--;LLL:EXT:cms/locallang_tca.xml:fe_users.tabs.options, lockToDomain;;;;1-1-1, TSconfig;;;;2-2-2,
-			--div--;LLL:EXT:cms/locallang_tca.xml:fe_users.tabs.access, starttime, endtime,
-			--div--;LLL:EXT:cms/locallang_tca.xml:fe_users.tabs.extended
-		')
-);
+
+t3lib_div::loadTCA('fe_users');
+t3lib_extMgm::addTCAcolumns('fe_users',$tempCols);
+$TCA['fe_users']['feInterface']['fe_admin_fieldList'].=',gtc';
+t3lib_extMgm::addToAllTCATypes('fe_users','gtc');
 
 // category
 //$TCA['sys_dmail_category']['interface'] = array(
